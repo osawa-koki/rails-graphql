70873b45cfa6d65e6ced108b453704129c832df5
osawa-koki <koki@osawa.dev>
2024-12-24 20:13:47 +0900

    add sample code

--- server/app/graphql/types/query_type.rb
+++ server/app/graphql/types/query_type.rb
@@ -21,11 +21,9 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :hello, String, null: false, description: "Hello, GraphQL!"
+    def hello
+      "Hello, GraphQL!"
     end
   end
 end
